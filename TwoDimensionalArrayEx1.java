8cefc215b4f288081b15f1518dc5f110abbbe514
YalingZheng <yaling.zheng@gmail.com>
2020-01-15 19:25:40 -0500

    More questions added

--- TwoDimensionalArrayEx1.java
+++ TwoDimensionalArrayEx1.java
@@ -16,7 +16,13 @@ public class CityDistances{
         String[] cities = {"Chicago", "Boston", "NewYork"};
         // Define a two-dimensional matrix to represent the distance table
         
+        // Get distance between Chicago and Boston (access a two dimensional array)
+        System.out.println("The distance between Chicago and Boston is ");
+        // Is the following method allowed ?
+        int distancesver2[][];
+        distancesver2 = new int[3][3];
+        // Copy distances to distancesver2 (deep copy, not a shallow copy)
         
             
-        }
+       }
 }

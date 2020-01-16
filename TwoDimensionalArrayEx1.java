fb1b66710532193df402cb42c30cbec566e8db94
YalingZheng <yaling.zheng@gmail.com>
2020-01-15 19:18:12 -0500

    Two Dimension Array Java

---
+++ TwoDimensionalArrayEx1.java
@@ -0,0 +1,22 @@
+/*
+A distance table  
+          Chicago Boston NewYork
+  Chicago   0      983    787
+  Boston   983     0      214
+  NewYork  787    214     0
+
+
+*/
+
+public class CityDistances{
+
+     public static void main(String []args){
+        System.out.println("City Distance Example");
+        // Define a matrix representating the distance between cities
+        String[] cities = {"Chicago", "Boston", "NewYork"};
+        // Define a two-dimensional matrix to represent the distance table
+        
+        
+            
+        }
+}

d307e7c53a66b57cc9af10d1a088e0c5ba3be68d
YalingZheng <yaling.zheng@gmail.com>
2020-01-15 20:42:22 -0500

    Verify Sudoku Solution

---
+++ VerifySudokuSolution.java
@@ -0,0 +1,28 @@
+/*
+Verify a sudoku solution
+Need to verify the rows, the columns, and the boxes
+Illustrate how to verify row
+Ask them to practise column and box 
+*/
+
+import java.util.Scanner;
+
+public class VerifySudokuSolution{
+
+     public static void main(String []args){
+        // Define a scannner, explain this one 
+        // Please refer https://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
+        Scanner scanner = new Scanner(System.in); 
+        // get the number of points
+        System.out.println("Please enter the sudoku solution: ");
+        // enter 81 digits
+        
+        // For each row, check whether it contains nine different digits. 
+
+        // For each column, check whether it contains nine different digits.  
+
+        // For each box, check whether it contains nine different digits
+        
+        return;  
+    }
+}

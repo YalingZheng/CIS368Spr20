203baaec98bc78bcbb28dae42893087eb2304e20
YalingZheng <yaling.zheng@gmail.com>
2020-01-15 21:10:03 -0500

    More stuff put there

--- VerifySudokuSolution.java
+++ VerifySudokuSolution.java
@@ -16,7 +16,18 @@ public class VerifySudokuSolution{
         // get the number of points
         System.out.println("Please enter the sudoku solution: ");
         // enter 81 digits
+        int[][] sudokudigits = new int[9][9];
+        for (int row = 0 ; row < 9 ; row++)
+            for (int col = 0; col < 9; col ++)
+                sudokudigits[row][col] = scanner.nextInt();
         
+        System.out.println("The input sudoku is: ");
+        for (int row = 0 ; row < 9 ; row++){
+            System.out.println();
+            for (int col = 0; col < 9; col ++){
+                System.out.print(sudokudigits[row][col] + " ");
+            }
+        }
         // For each row, check whether it contains nine different digits. 
 
         // For each column, check whether it contains nine different digits.  

c0358ba5fb0c47b189c29806db347d905f6b9a1a
YalingZheng <yaling.zheng@gmail.com>
2020-01-15 20:36:53 -0500

    Find Closest Pair

---
+++ FindClosestPair.java
@@ -0,0 +1,26 @@
+/*
+Finding the closest pair 
+Enter a number of points, and find the two closest pair of points 
+This one show how to scan into 6 points
+*/
+
+import java.util.Scanner;
+
+public class FindClosestPair{
+
+     public static void main(String []args){
+        // Define a scannner, explain this one 
+        // Please refer https://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
+        Scanner scanner = new Scanner(System.in); 
+        // get the number of points
+        System.out.println("Please enter the number of 2D points: ");
+        int numpoints = scanner.nextInt();
+        // For each point, get the inputs of its x-value and y-value 
+        
+        // Use a brute-force method to find the pair of points - extra memory used to remember this two points
+        
+        // Output these two points
+        
+        return;  
+    }
+}

4a212c5d435876e9109d108625aae5de963a7e88
YalingZheng <yaling.zheng@gmail.com>
2020-01-28 11:47:43 -0500

    match phone numbers

---
+++ StringMatchingExample.java
@@ -0,0 +1,15 @@
+public class StringMatchingExample {
+
+  public static void main(String[] args){
+    // What does the following evaluate? 
+    "216-333-4449".matches("\\d{3}-\\d{3}-\\d{4}");
+    
+    // How do you match format "2163334449"?
+    
+    // how do you match format "216.333.4449"?
+    
+    // how do you match format "(216)333-4449"? 
+  
+  }
+
+}

4fd0677f49bd5b7a91bc77cc333eb0d25f678d4c
YalingZheng <yaling.zheng@gmail.com>
2020-02-13 08:33:20 -0500

    test class

---
+++ DesignPatterns/SingletonDP/TestGlobalSettings.java
@@ -0,0 +1,36 @@
+package SingletonDP;
+
+import java.util.ArrayList;
+
+public class TestGlobalSettings {
+	private static ArrayList<Thread> arrThreads = new ArrayList<Thread>();
+	public static void main(String[] args) {
+
+		 
+		// random wait for a time
+		Agent[] agents = new Agent[10];
+		for (int i = 0; i < 9; i++) {
+			agents[i] = new Agent(i);
+			 arrThreads.add(agents[i]);
+		}
+
+		for (int i = 0; i < 9; i++) {
+			agents[i].start();
+		}
+		
+		for (int i = 0; i < arrThreads.size(); i++) 
+        {
+            try {
+				arrThreads.get(i).join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} 
+        } 
+		
+		SingletonGlobalSettings gs = SingletonGlobalSettings.getInstance();
+		SingletonGlobalSettings gs2 = gs.clone();
+		System.out.println("The cloned SingletonGlobalSettings is " + gs2);
+		
+
+	}
+}

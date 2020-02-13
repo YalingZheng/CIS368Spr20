eb8a0ba20b2e6b384ac8b43f22dd4c4f84582c60
YalingZheng <yaling.zheng@gmail.com>
2020-02-13 08:24:27 -0500

    Test class for this observer design pattern

--- DesignPatterns/ObserverDesignPattern/WeatherStation.java
+++ DesignPatterns/ObserverDesignPattern/WeatherStation.java
@@ -9,5 +9,6 @@ public class WeatherStation {
 		weatherData.setMeasurements(80, 65, 30.4f);
 		weatherData.setMeasurements(82, 70, 29.2f);
 		weatherData.setMeasurements(78, 90, 29.2f);
+		weatherData.setMeasurements(68, 20, 50.1f);
 	}
 }

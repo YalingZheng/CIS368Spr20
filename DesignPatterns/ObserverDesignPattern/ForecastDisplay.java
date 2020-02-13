9a3e7223270ce8a323e036b953add7ebe9205243
YalingZheng <yaling.zheng@gmail.com>
2020-02-13 08:20:04 -0500

    c

---
+++ DesignPatterns/ObserverDesignPattern/ForecastDisplay.java
@@ -0,0 +1,23 @@
+package ObserverDesignPattern;
+
+public class ForecastDisplay implements Observer, DisplayElement {
+	private float temperature;
+	private float humidity;
+	private Subject weatherData;
+
+	public ForecastDisplay(Subject weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+
+	public void update(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		display();
+	}
+
+	public void display() {
+		System.out.println("Forecast - In Development");
+		// Forecast based on some rules and/or history data
+	}
+}

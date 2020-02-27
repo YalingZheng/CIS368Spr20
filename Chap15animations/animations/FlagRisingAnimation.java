bcd19c756a021b0d1d7ccb790fac6a8ba97c7014
YalingZheng <yaling.zheng@gmail.com>
2020-02-27 12:06:19 -0500

    c

---
+++ Chap15animations/animations/FlagRisingAnimation.java
@@ -0,0 +1,43 @@
+package animations;
+
+import javafx.animation.PathTransition;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class FlagRisingAnimation extends Application {
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {
+    // Create a pane 
+    Pane pane = new Pane();
+    
+    // Add an image view and add it to pane
+    ImageView imageView = new ImageView("image/us.jpg");
+    pane.getChildren().add(imageView);
+    
+    // Create a path transition 
+    PathTransition pt = new PathTransition(Duration.millis(10000),
+      new Line(100, 600, 100, 200), imageView);
+    // Repeat twice and stop
+    pt.setCycleCount(2);
+    pt.play(); // Start animation 
+    
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(pane, 800, 800);
+    primaryStage.setTitle("FlagRisingAnimation"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+  }
+  
+  /**
+   * The main method is only needed for the IDE with limited
+   * JavaFX support. Not needed for running from the command line.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}

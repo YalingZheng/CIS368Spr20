5fbd7157dae42d2204b312aaad92cfcc539048e4
YalingZheng <yaling.zheng@gmail.com>
2020-03-31 22:25:25 -0400

    Movie

--- HW6/Movie.java
+++ HW6/Movie.java
@@ -1,45 +1,62 @@
 import java.util.ArrayList;
 
 /**
- * Datatype for Movies. Each movie has a name and a list of
- * Actors
+ * Datatype for Movies. Each movie has a name and a list of Actors
  *
  */
 public class Movie {
 	/**
 	 * Name of the Movie -- assumed to be unique
 	 */
-	public String name;
-	private ArrayList<Actor> myActors;
-		
-	public Movie(String title)
-	{
-		name = title;
-		myActors = new ArrayList<Actor>();
+	private String name;
+	private ArrayList<String> Actors;
+	private double rating;
+
+	public String getName() {
+		return name;
 	}
-	/**
-	 * Add an Actor to our list of cast members
-	 * @param person Actor that appeared in this Movie
-	 */
-	public void addActor(Actor person) {
-		myActors.add(person);
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<String> getActors() {
+		return Actors;
+	}
+
+	public void setActors(ArrayList<String> myActors) {
+		this.Actors = myActors;
+	}
+
+	public Movie(String title) {
+		name = title;
+		Actors = new ArrayList<String>();
 	}
 
 	/**
-	 * Return an iterator over all Actors in this Movie
-	 * @return an iterator over all Actors in this Movie
+	 * Add an Actor to our list of cast members
+	 * 
+	 * @param person Actor that appeared in this Movie
 	 */
-	public Iterable<Actor> getActors()
-	{
-		return myActors;
+	public void addActor(String person) {
+		Actors.add(person);
 	}
+
+
 	/**
-	 * The name of the Movie is assumed to be unique, so it
-	 * is used as a HashCode
+	 * The name of the Movie is assumed to be unique, so it is used as a HashCode
 	 * 
 	 * @see java.lang.Object#hashCode()
 	 */
 	public int hashCode() {
 		return name.hashCode();
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
 	}
 }
